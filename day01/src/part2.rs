a2d4e20cd300c33515776df32be0b2ac8ed7c750
DeCaelo <ludovic.cleuet@gmail.com>
2023-12-02 17:03:44 +0100

    day01 part2

---
+++ day01/src/part2.rs
@@ -0,0 +1,73 @@
+use crate::custom_error::AocError;
+use phf::phf_map;
+
+static DIGITS: phf::Map<&'static str, u32> = phf_map! {
+    "one" => 1,
+    "two" => 2,
+    "three" => 3,
+    "four" => 4,
+    "five" => 5,
+    "six" => 6,
+    "seven" => 7,
+    "eight" => 8,
+    "nine" => 9,
+};
+
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let output = input.lines().map(process_line).sum::<u32>();
+
+    Ok(output.to_string())
+}
+
+fn process_line(line: &str) -> u32 {
+    let mut it = (0..line.len()).filter_map(|index| {
+        let reduced_line = &line[index..];
+        let from_letters = DIGITS
+            .entries()
+            .find_map(|(digit, num)| reduced_line.starts_with(digit).then_some(*num));
+        from_letters.or(reduced_line.chars().next().unwrap().to_digit(10))
+    });
+    let first = it.next().expect("should be a number");
+
+    match it.last() {
+        Some(num) => first * 10 + num,
+        None => first * 10 + first,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    #[case("fivezg8jmf6hrxnhgxxttwoneg", 51)]
+    fn line_test(#[case] line: &str, #[case] expected: u32) {
+        assert_eq!(expected, process_line(line))
+    }
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
+        Ok(())
+    }
+}
+
+// phf for a static map
+// .then_some for returning a value after testing a boolean
+// .or for returning one option or another.
